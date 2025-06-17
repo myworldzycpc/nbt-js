1b117552bf84c40749f303a9968946a1c3bb4de2
myworldzycpc <67212814+myworldzycpc@users.noreply.github.com>
2025-06-17 23:36:31 +0800

    Update parser-tests.js

--- test/parser-tests.js
+++ test/parser-tests.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { parseNbtString, NbtObject, NbtList } = require('../src/nbt');
+const { parseNbtString, NbtObject, NbtList } = require('../src/snbt');
 
 describe('NBT Parser', () => {
   it('should parse simple compound', () => {
